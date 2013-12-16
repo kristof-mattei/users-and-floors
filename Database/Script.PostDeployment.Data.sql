5ae694bd3edb219773908ed193933b1250f4b095
Kristof Mattei <kristof@kristofmattei.be>
2013-12-16 21:24:01 +0100

    Added test to prove that spaces are bad

--- Database/Script.PostDeployment.Data.sql
+++ Database/Script.PostDeployment.Data.sql
@@ -3,7 +3,8 @@
 VALUES
     ('Floor 1', 'John Boston'),
     ('Floor 2', 'Joe Phillips'),
-    ('Floor 3', 'Randy Ford')
+    ('Floor 3', 'Randy Ford'),
+    ('Floor 4', 'Solomon Sherman')
 
 GO
 INSERT INTO 
@@ -11,25 +12,9 @@ INSERT INTO
 VALUES
     ('Josiah Benton', 'Floor 1'), 
     ('Phillip Goodman', 'Floor 1'), 
-    ('Declan Turner', 'Floor 1'), 
-    ('Clinton Mueller', 'Floor 1'), 
-    ('Dean Zimmerman', 'Floor 1'), 
-    ('Harrison Bailey', 'Floor 1'), 
-    ('Myles Schwartz', 'Floor 1'),
-    ('Joel Carpenter', 'Floor 1'), 
-    ('Oleg Gonzales', 'Floor 1'),
-    ('Nash Bonner', 'Floor 1'),
-    ('Gabriel Shaffer', 'Floor 2'),
-    ('Elijah Houston', 'Floor 2'),
-    ('Amal Kim', 'Floor 2'), 
-    ('Sawyer Pennington', 'Floor 2'),
-    ('Lawrence Hebert', 'Floor 2'),
-    ('Xander Carroll', 'Floor 2'),
-    ('Brett Park', 'Floor 2'), 
-    ('Todd Hayes', 'Floor 2'),
     ('Tanek Frederick', 'Floor 2'),
     ('Len Hinton', 'Floor 2'),
     ('Bruno Sweet', 'Floor 3'), 
     ('Clarke Schmidt', 'Floor 3'), 
-    ('Colin Underwood', 'Floor 3'), 
-    ('Solomon Sherman', 'Floor 3');
+    ('Amal Kim', 'Floor 4'),
+    ('Colin Underwood', 'Floor 4 ') -- to the reader, the error is here, notice the space
--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -1,14 +1,25 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-
-namespace Tests
+﻿namespace Tests
 {
+    using System.Collections.Generic;
+    using System.Linq;
+    using DataAccess;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
     [TestClass]
     public class UnitTest1
     {
         [TestMethod]
         public void TestMethod1()
         {
+            using (var usersAndFloorsEntities = new UsersAndFloorsEntities())
+            {
+                // let's find all the floors being used:
+                List<string> usedFloors = usersAndFloorsEntities.Users.Select(e => e.FloorId).Distinct().ToList();
+
+                // however...
+                List<string> usedFloors2 = usersAndFloorsEntities.Users.ToList().Select(e => e.FloorId).Distinct().ToList();
+
+            }
         }
     }
 }
